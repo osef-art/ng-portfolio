94950ed6ce539f4fe512ef1babe20c039bc62d1b
adam <66301048+aadedjou@users.noreply.github.com>
2021-07-29 02:04:20 +0200

    Add default rooting path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ArtPageComponent } from './art-page/art-page.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AnimsPageComponent } from './anims-page/anims-page.component';
-import { GamesPageComponent } from './games-page/games-page.component';
-import { MusicPageComponent } from './music-page/music-page.component';
+import { ArtPageComponent } from './pages/art-page/art-page.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AnimsPageComponent } from './pages/anims-page/anims-page.component';
+import { GamesPageComponent } from './pages/games-page/games-page.component';
+import { MusicPageComponent } from './pages/music-page/music-page.component';
 
 const routes: Routes = [
+  { path: "", component: HomePageComponent },
   { path: "hi", component: AboutMeComponent },
   { path: "art", component: ArtPageComponent },
   { path: "home", component: HomePageComponent },
