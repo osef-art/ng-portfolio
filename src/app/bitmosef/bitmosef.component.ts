5a944b73e23692f665a4c28b0f4c7262c1c31a90
adam <66301048+aadedjou@users.noreply.github.com>
2021-07-29 02:08:33 +0200

    Implement Bitmosef in home page

--- src/app/bitmosef/bitmosef.component.ts
+++ src/app/bitmosef/bitmosef.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-bitmosef',
+  selector: 'bitmosef',
   templateUrl: './bitmosef.component.html',
   styleUrls: ['./bitmosef.component.scss']
 })
-export class BitmosefComponent implements OnInit {
+export class BitmosefComponent {
+  hidden : boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  hideBitmoji() {
+    this.hidden = true;
   }
-
 }
