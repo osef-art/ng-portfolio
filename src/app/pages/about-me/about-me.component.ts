f23286bbe1b919699f5bbdc67e26529b0d01f7e8
adam <66301048+aadedjou@users.noreply.github.com>
2021-07-30 03:52:00 +0200

    Update title and favicon for bio page

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AboutMeComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("hi! 👉🏾👈🏾");
   }
-
 }
