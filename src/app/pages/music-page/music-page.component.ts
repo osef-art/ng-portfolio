31a1cd3f3bbb4d33eb3f1a94d0f1f7446e65f081
adam <66301048+aadedjou@users.noreply.github.com>
2021-07-30 03:55:34 +0200

    Update title & favicon for music page

--- src/app/pages/music-page/music-page.component.ts
+++ src/app/pages/music-page/music-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-music-page',
+  selector: 'music-page',
   templateUrl: './music-page.component.html',
   styleUrls: ['./music-page.component.scss']
 })
-export class MusicPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MusicPageComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("music.");
   }
-
 }
