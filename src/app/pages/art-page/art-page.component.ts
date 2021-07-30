79f3f2216341de341c7f250b6aaf215bf882529f
adam <66301048+aadedjou@users.noreply.github.com>
2021-07-30 03:53:54 +0200

    Update title & favicon for art page

--- src/app/pages/art-page/art-page.component.ts
+++ src/app/pages/art-page/art-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-art-page',
   templateUrl: './art-page.component.html',
   styleUrls: ['./art-page.component.scss']
 })
-export class ArtPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ArtPageComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("art.");
   }
-
 }
