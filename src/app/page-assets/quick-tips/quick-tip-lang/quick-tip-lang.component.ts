e680911f783f03468cd01b6faca409b5e6c517ab
adam <66301048+aadedjou@users.noreply.github.com>
2021-07-31 04:24:27 +0200

    Implement language switching in tip

--- src/app/page-assets/quick-tips/quick-tip-lang/quick-tip-lang.component.ts
+++ src/app/page-assets/quick-tips/quick-tip-lang/quick-tip-lang.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { Lang } from 'src/models/models';
 
 @Component({
   selector: 'quick-tip-lang',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./quick-tip-lang.component.scss']
 })
 export class QuickTipLangComponent {
+  get mainLang() {
+    return AppComponent.language;
+  }
+
+  get Lang() : typeof Lang {
+    return Lang;
+  }
+
+  get langKeys() {
+    return Object.values(Lang);
+  }
+
+  setLanguage(lang : Lang) {
+    AppComponent.language = lang;
+  }
 }
