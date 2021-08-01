3b29888ddbf2e5b6890619beffe50a209f9cb270
adam <66301048+aadedjou@users.noreply.github.com>
2021-08-01 21:24:24 +0200

    Update data in models

---
+++ src/models/gamecard.ts
@@ -0,0 +1,131 @@
+import { TextContent } from "./models";
+
+export enum ProgLang {
+  LIBGDX = "LibGDX",
+  JAVA = "Java",
+  JS = "Javascript",
+}
+
+export class GameCard {
+  title: string;
+  desc: TextContent;
+  date: Date;
+
+  isBeta: boolean = false;
+  devProg: number = 0;
+  languages: ProgLang[];
+
+  thumbnailUrl: string;
+  thumbnailAnimatedUrl!: string;
+
+  constructor(title: string, desc: TextContent, thumbnailName: string, languages: ProgLang[], date: Date, devProg?: number) {
+    this.thumbnailUrl = "assets/thumbnails/" + thumbnailName + "-tbn.png";
+    if (devProg) this.devProg = devProg;
+    else this.isBeta = true;
+    this.languages = languages;
+    this.title = title;
+    this.desc = desc;
+    this.date = date;
+  }
+}
+
+export class GameCardData {
+  static cards: GameCard[] = [];
+
+  constructor() {
+    GameCardData.cards = [];
+
+    GameCardData.cards.push(
+      new GameCard(
+        "ministick.",
+        new TextContent(
+          "An addictive, infinite and super-fast beat-them all.\n\
+           Let's see how far you can go !"
+        ),
+        "ministick",
+        [ProgLang.JAVA],
+        new Date(Date.UTC(2020, 3, 0, 0, 0, 0)), .8
+      )
+    )
+
+    GameCardData.cards.push(
+      new GameCard(
+        "ministick.",
+        new TextContent(
+          "The very first playable version of ministick !\n\
+          The controls and physics have quite changed since, but this one was\
+          more focused on the different enemies abilities !"
+        ),
+        "ministick-js",
+        [ProgLang.JS],
+        new Date(Date.UTC(2019, 9, 0, 0, 0, 0)), .6
+      )
+    )
+
+    GameCardData.cards.push(
+      new GameCard(
+        "TETRIS !",
+        new TextContent(
+          "A simple Tetris made with Javascript."
+        ),
+        "tetris",
+        [ProgLang.JS],
+        new Date(Date.UTC(2018, 5, 0, 0, 0, 0)), 1
+      )
+    )
+
+    GameCardData.cards.push(
+      new GameCard(
+        "Lost in Space",
+        new TextContent(
+          "This was the final project I had to return in my first year\
+          in computer science studies.\n It got me the maximal grade 😏"
+        ),
+        "lost-in-space",
+        [ProgLang.JS],
+        new Date(Date.UTC(2018, 4, 0, 0, 0, 0)), 1
+      )
+    )
+
+    GameCardData.cards.push(
+      new GameCard(
+        "ministick-moves",
+        new TextContent(
+          "a quick sandbox environment made to test new moves and mechanics for ministick.v2 !"
+        ),
+        "ministick-moves",
+        [ProgLang.LIBGDX, ProgLang.JAVA],
+        new Date(Date.UTC(2021, 2, 0, 0, 0, 0))
+      )
+    )
+
+    GameCardData.cards.push(
+      new GameCard(
+        "KAPS",
+        new TextContent(
+          "a second version of KAPS made with LibGDX. contains a bunch of new sidekicks !"
+        ),
+        "kaps-v2-3",
+        [ProgLang.LIBGDX, ProgLang.JAVA],
+        new Date(Date.UTC(2021, 1, 0, 0, 0, 0))
+      )
+    )
+
+    GameCardData.cards.push(
+      new GameCard(
+        "KAPS",
+        new TextContent(
+          "A 'Dr. Mario'-like colorful mini-game.\n\
+          Match the colored capsules and get rid of every germ in the grid ! 🧪"
+        ),
+        "kaps-2",
+        [ProgLang.JAVA],
+        new Date(Date.UTC(2019, 12, 0, 0, 0, 0)), .5
+      )
+    )
+
+    GameCardData.cards.sort((a: GameCard, b: GameCard) => {
+      return b.date.getTime() - a.date.getTime();
+    });
+  }
+}
