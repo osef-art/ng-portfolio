717c42efeff3efc7cf8ed596fe8fa296a60d83a2
adam <66301048+aadedjou@users.noreply.github.com>
2021-08-01 21:11:52 +0200

    Implement game card list in games page

--- src/app/pages/games-page/games-page.component.ts
+++ src/app/pages/games-page/games-page.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { GameCard, GameCardData } from 'src/models/gamecard';
+import { Kind } from 'src/models/models';
 
 @Component({
-  selector: 'app-games-page',
+  selector: 'games-page',
   templateUrl: './games-page.component.html',
   styleUrls: ['./games-page.component.scss']
 })
 export class GamesPageComponent {
+  pinnedGames : GameCard[] = [];
+  gameList : GameCard[] = [];
+
   constructor(private titleService: Title) {
+    new GameCardData();
+    this.pinnedGames = GameCardData.cards.slice(0, 4);
+    this.gameList = GameCardData.cards;
     this.titleService.setTitle("games.");
+    AppComponent.setPageKind(Kind.GAMES);
   }
 }
