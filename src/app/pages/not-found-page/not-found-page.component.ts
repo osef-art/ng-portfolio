d5166e146c26744eb9482e8b0db0a04cb42e11ba
adam <66301048+aadedjou@users.noreply.github.com>
2021-08-04 08:17:06 +0200

    Add return button to 404 page

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
+import { Kind } from 'src/models/models';
 
 @Component({
   selector: 'not-found-page',
@@ -9,5 +10,12 @@ import { AppComponent } from 'src/app/app.component';
 export class NotFoundPageComponent {
   constructor() {
     AppComponent.scrollToTop();
+  }
+
+  get buttonLabel() : string {
+    if (AppComponent.pageKind != Kind.NONE) {
+      return "Return to " + AppComponent.pageKind;
+    }
+    return "Previous page";
   }
 }
