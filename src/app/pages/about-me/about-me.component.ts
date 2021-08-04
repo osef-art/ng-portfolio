d2708b08c35608e3694edb8d51bf859b7c876b2e
adam <66301048+aadedjou@users.noreply.github.com>
2021-08-04 08:22:45 +0200

    Fill bio component

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { HomePageComponent } from '../home-page/home-page.component';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
   constructor(private titleService: Title) {
     this.titleService.setTitle("hi! 👉🏾👈🏾");
+  }
+
+  ngOnInit(): void {
+    HomePageComponent.buildWave(60, 60);
   }
 }
