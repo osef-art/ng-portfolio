04ab38b8417f1f8f479968c351585ddb4a2a54ac
adam <66301048+aadedjou@users.noreply.github.com>
2021-08-04 08:18:31 +0200

    Add routes for each game and a default gateway page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ArtPageComponent } from './pages/art-page/art-page.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GamePageComponent } from './pages/games-page/game-page/game-page.component';
 import { AnimsPageComponent } from './pages/anims-page/anims-page.component';
 import { GamesPageComponent } from './pages/games-page/games-page.component';
 import { MusicPageComponent } from './pages/music-page/music-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
@@ -16,6 +18,11 @@ const routes: Routes = [
   { path: "anims", component: AnimsPageComponent },
   { path: "music", component: MusicPageComponent },
   { path: "games", component: GamesPageComponent },
+  {
+    path: "games/:id",
+    component: GamePageComponent
+  },
+  { path: "**", component: NotFoundPageComponent },
 ];
 
 @NgModule({
