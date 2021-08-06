74fdc322182f6ae8c14ca8d01ae8bbc947e4a9c6
adam <82466060+osef-art@users.noreply.github.com>
2021-08-07 01:46:46 +0200

    Memorize current language when navigating/refreshing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,15 @@ export class AppComponent {
   static pageKind : Kind = Kind.NONE;
   static language : Lang = Lang.ENG;
 
+  constructor() {
+    var currentLang = localStorage.getItem("lang");
+    if (currentLang) AppComponent.language = (<any>Lang)[currentLang];
+    else localStorage.setItem("lang", AppComponent.language);
+  }
+
   static switchLanguage() {
     this.language = this.language == Lang.ENG ? Lang.FRA : Lang.ENG;
+    localStorage.setItem("lang", this.language);
   }
 
   static scrollToTop() {
