26bf3f39675e4c15099933eaa1fb0bc9efba8ba3
adam <82466060+osef-art@users.noreply.github.com>
2021-08-09 15:57:22 +0200

    Add a custom page for each game !

--- src/app/pages/games-page/game-page/game-page.component.ts
+++ src/app/pages/games-page/game-page/game-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GithubRequestService } from 'src/app/services/http-request.service';
+import { MdToHtmlParserService } from 'src/app/services/md-to-html-parser.service';
+import { AppComponent } from 'src/app/app.component';
+import { Kind } from 'src/models/models';
+import * as marked from 'marked';
+import { PageScrollerService } from 'src/app/services/page-scroller.service';
 
 @Component({
   selector: 'game-page',
@@ -7,12 +13,43 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./game-page.component.scss']
 })
 export class GamePageComponent implements OnInit {
-  name !: string;
+  gameId !: string;
+  readmeHTML !: string;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private githubRequester : GithubRequestService,
+    private scroller : PageScrollerService,
+    private parser : MdToHtmlParserService,
+    private route: ActivatedRoute,
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     const name = this.route.snapshot.paramMap.get('id');
-    if (name) this.name = name;
+    if (name) this.gameId = name;
+
+    this.scroller.scrollToTop();
+    AppComponent.setPageKind(Kind.GAMES);
+    this.getGameReadmeFile();
+  }
+
+  get Kind() : typeof Kind {
+    return Kind;
+  }
+
+  getGameReadmeFile() {
+    const githubURL : string = "(https://raw.githubusercontent.com/osef-art/" + this.gameId + "/main/$1";
+
+    this.githubRequester
+      .getReadMeFrom(this.gameId)
+      .subscribe(data => {
+        this.readmeHTML = this.parser.parsedWithRules(data, {
+          [githubURL] : /\(([a-zA-Z0-9_\-/]+\.(png|jpg|gif))/
+        });
+        this.readmeHTML = marked.setOptions({}).parse(this.readmeHTML);
+      });
+  }
+
+  scrollTo(elm : HTMLElement) {
+    this.scroller.scrollTo(elm);
   }
 }
