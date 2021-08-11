f4bbe4837214b044ffbbe5715da2dfd57293b615
adam <82466060+osef-art@users.noreply.github.com>
2021-08-11 17:45:20 +0200

    Get intro text translatable

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { Title } from '@angular/platform-browser';
-import { Kind } from 'src/models/models';
+import { Kind, Lang, TranslatableText } from 'src/models/models';
 import { PageScrollerService } from 'src/app/services/page-scroller.service';
+import { CustomParserService } from 'src/app/services/custom-parser.service';
 
 @Component({
   selector: 'app-home-page',
@@ -10,15 +11,27 @@ import { PageScrollerService } from 'src/app/services/page-scroller.service';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  constructor(private titleService: Title, private scroller : PageScrollerService) {
+  intro !: TranslatableText;
+
+  constructor(
+    private titleService: Title,
+    private scroller : PageScrollerService,
+    private parser : CustomParserService,
+  ) {
     this.titleService.setTitle("welcome.");
     AppComponent.resetPageKind();
   }
 
   ngOnInit() {
     HomePageComponent.buildWave(60, 60);
+    this.parser.setRules({
+      '<span class="white">$1</span>': /\*([^\s]([^\*]+)[^\s])\*/,
+      '<a href="/$2" routerLink="/$2" class="$2-link">$1</a>': /\[([a-zé\s]+)\]\(([a-z]+)\)/,
+    });
+    this.intro = this.randomIntro;
   }
 
+  // into service
   static buildWave(w: number, h: number) {
     const a = h / 4;
     const y = h / 2;
@@ -87,5 +100,43 @@ export class HomePageComponent implements OnInit {
 
   get Kind() : typeof Kind {
     return Kind;
+  }
+
+  private randomizedNav(lang : Lang) : string {
+    var sentence : string = "";
+    const order : Kind[] = Object.values(Kind);
+    const skills : { [key in Kind]: TranslatableText } = {
+      [Kind.NONE] : new TranslatableText(),
+      [Kind.ART] : new TranslatableText('designing', 'graphisme'),
+      [Kind.GAMES] : new TranslatableText('game developping', 'développement'),
+      [Kind.ANIMS] : new TranslatableText('animating', 'animation'),
+      [Kind.MUSIC] : new TranslatableText('producing', 'composition'),
+    }
+
+    order.shift();
+    order.sort( () => .5 - Math.random() );
+    order.forEach(kind =>
+      sentence += (
+        (kind == order[order.length-1] ? new TranslatableText('or', 'ou').in(lang) + " " : "") +
+        (lang == Lang.FRA ? (kind == Kind.ANIMS ? "d'" : "de ") : "") +
+        ("[" + skills[kind].in(lang) + "](" + kind + ")") +
+        (kind == order[order.length-1] ? " !" : ", ")
+      )
+    );
+    return sentence;
+  }
+
+  get randomIntro() : TranslatableText {
+    return new TranslatableText(
+      "*glad you got there.* feel free to check my latest works in " +
+      this.randomizedNav(Lang.ENG),
+      "*content de vous voir.* venez jeter un oeil\
+      à mes dernières créations en matière " +
+      this.randomizedNav(Lang.FRA),
+    );
+  }
+
+  get introHtml() : string {
+    return this.parser.parsed(this.intro);
   }
 }
